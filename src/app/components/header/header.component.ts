3bcbc44588ba9db0a8e7252709ea4802240559b2
Flaviabel <flaviamanfredi521@gmail.com>
2023-04-07 21:10:00 +0200

    add service ui

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   title: string = 'Task List';
-  toogleAddTask(){
-    console.log("toogleAddTask")
+  showAddTask: boolean = true;
+  subscription?: Subscription;
+
+  constructor(
+    private uiService: UiService
+  ) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
+
+  ngOnInit(): void { }
+
+  toogleAddTask() {
+    this.uiService.toggleAddTask();
   }
 
 }
